616017529de1080b0c2c00271b4ba7861aedc6c0
abdurrahman sale <abdurrahmansale.123@gmail.com>
2026-09-24 00:34:00 +0100

    latest version

--- app/(root)/dashboard/assessments/page.tsx
+++ app/(root)/dashboard/assessments/page.tsx
@@ -12,3 +12,35 @@ const page = () => {
 }
 
 export default page
+
+
+// import { DashboardLayoutWrapper } from "@/app/components/dashboard/DashboardLayoutWrapper";
+// import ExamPracticePage from "@/app/components/dashboard/assessments/exams/ExamPracticePage";
+
+// const page = () => {
+//   return (
+//     <div>
+//     <DashboardLayoutWrapper>
+//       <ExamPracticePage />
+//     </DashboardLayoutWrapper>
+//     </div>
+//   )
+// }
+
+// export default page
+
+
+// import { DashboardLayoutWrapper } from "@/app/components/dashboard/DashboardLayoutWrapper";
+// import ExamReportPage from "@/app/components/dashboard/assessments/report/report";
+
+// const page = () => {
+//   return (
+//     <div>
+//     <DashboardLayoutWrapper>
+//       <ExamReportPage />
+//     </DashboardLayoutWrapper>
+//     </div>
+//   )
+// }
+
+// export default page
